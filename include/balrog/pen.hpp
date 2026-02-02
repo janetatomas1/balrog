ef8310b3a8125a4ee59287b6229fc518157d743f
janetatomas1 <tomas.janeta@proton.me>
2026-02-02 16:48:38 +0100

    Pen

---
+++ include/balrog/pen.hpp
@@ -0,0 +1,14 @@
+
+
+#pragma once
+
+#include <array>
+
+namespace balrog {
+
+struct Pen {
+    std::array<float, 4> color_ = {1.0f, 0.0f, 0.0f, 1.0f};
+    float thickness_ = 1.0f;
+};
+
+} // namespace balrog
